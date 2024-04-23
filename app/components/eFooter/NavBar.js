adfea97c0876daff1b6f462b47f338ce66bbf2dc
thuanpv1 <thuanpv@egt.vn>
2024-04-23 17:08:44 +0700

    fix bug

--- app/components/eFooter/NavBar.js
+++ app/components/eFooter/NavBar.js
@@ -2,4 +2,9 @@ import styled from 'styled-components';
 
 export default styled.div`
   text-align: center;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 3em 0;
+  border-top: 1px solid #666;
 `;
--- app/components/eFooter/index.js
+++ app/components/eFooter/index.js
@@ -3,32 +3,21 @@ import { FormattedMessage } from 'react-intl';
 
 import A from 'components/A';
 import LocaleToggle from 'containers/LocaleToggle';
-import Wrapper from './Wrapper';
 import messages from './messages';
 import NavBar from './NavBar';
 import HeaderLink from './HeaderLink';
 import styled from 'styled-components';
 
-const myDiv = styled.div`
-  text-align: center;
-
-`
-
 function Footer() {
   return (
-    <Wrapper>
       <NavBar>
         <HeaderLink to="/danhsach">
-          <FormattedMessage {...messages.danhSach} />
         </HeaderLink>
         <HeaderLink to="/goimon">
-          <FormattedMessage {...messages.goiMon} />
         </HeaderLink>
         <HeaderLink to="/thanhToan">
-          <FormattedMessage {...messages.thanhToan} />
         </HeaderLink>
       </NavBar>
-    </Wrapper>
   );
 }
 
